ac2515c6adb218c5f4364241d560a2a94f501b50
SEJSCloud <cloud504@naver.com>
2020-02-06 23:19:49 +0900

    Update 메뉴 선택창 로직 추가 및 개선

--- recipeHelper/src/main/java/model/dao/ThirdCategoryRepository.java
+++ recipeHelper/src/main/java/model/dao/ThirdCategoryRepository.java
@@ -2,8 +2,10 @@ package model.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+import model.domain.SecondCategory;
 import model.domain.ThirdCategory;
 
 public interface ThirdCategoryRepository extends CrudRepository<ThirdCategory, String> {
 	long count();
+	Iterable<ThirdCategory> findAllBySecondCategory(SecondCategory secondCategory);
 }
